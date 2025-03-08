4bb9c29af70833d0857f80f423554c7325b1556c
Denis Kovalev <kovalevdenis22@gmail.com>
2025-03-08 19:32:19 +0100

    Синхронизация репозитория с текущей структурой проекта

--- scripts/IInteractable.cs
+++ scripts/IInteractable.cs
@@ -2,8 +2,8 @@ using Godot;
 
 public interface IInteractable
 {
-	string GetInteractionHint();
-	bool CanInteract(Node source);
-	bool Interact(Node source);
-	float GetInteractionRadius();
+    string GetInteractionHint();
+    bool CanInteract(Node source);
+    bool Interact(Node source);
+    float GetInteractionRadius();
 }
